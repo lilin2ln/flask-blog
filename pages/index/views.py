ed482aef79afd684ad507b8a8ef4803a9cdf196b
lilin <lilin@capsheaf.com.cn>
2019-01-15 09:51:13 +0800

    #func-alter: 增加从数据库获取文章列表的功能

--- pages/index/views.py
+++ pages/index/views.py
@@ -9,8 +9,13 @@ class IndexView(BaseView):
         self.template_name = 'index/index.html'
 
     def get_data(self):
-        print api.get_article_list()
+
+        article_lists_res = api.get_article_list() #获取文章列表
+        article_lists = article_lists_res["detail"]
+
+
         return {"blog_name": "123",
                 "blog_description": "456",
                 "author": "wi",
-                "author_introduce": "testtesttest!"}
+                "author_introduce": "testtesttest!",
+                "article_lists": article_lists}
