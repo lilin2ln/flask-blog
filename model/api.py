67d7a5e9ac0b04267f601a250b863d05175a6b44
lilin <lilin@capsheaf.com.cn>
2019-01-15 10:50:33 +0800

    #func-alter: 增加首页的推荐文章列表信息获取；修改代码注释格式

--- model/api.py
+++ model/api.py
@@ -4,6 +4,7 @@
 # File: api.py
 from . import base
 
+
 def register(data):
     """register user account
     :param data: {
@@ -16,11 +17,42 @@ def register(data):
                  }
     :return: 成功-True/失败-False
     """
-    user_tb = base.Base("b_users") #实例化数据库操作基类
-    user_tb.connect() #连接数据库
-    res = user_tb.insert(data) #执行插入操作
-    user_tb.close() #关闭数据库连接
+    user_tb = base.Base("b_users")  # 实例化数据库操作基类
+    user_tb.connect()  # 连接数据库
+    res = user_tb.insert(data)  # 执行插入操作
+    user_tb.close()  # 关闭数据库连接
     return res
+
+
+def get_recommend_article_lists():
+    """获取推荐文章的简要信息
+    :return: {
+        "valid": True/False,
+        "error_msg": "",
+        "detail": [{
+            "uid": "文章uid",
+            "article_title": "文章标题",
+            "message": "文章标题中的图片名称",
+            "comments": "文章评论数",
+            "views": "文章查看数",
+            "add_time": "添加时间",
+            "is_top": "文章是否置顶",
+            "category_id": "文章类型",
+            "praises": "点赞数",
+            "is_recommend": "是否推荐"
+        }]
+    }
+    """
+    b_article_tb = base.Base("b_article")  # 实例化数据库操作基类
+    b_article_tb.connect()  # 连接数据库
+    res = b_article_tb.show({"is_recommend": 1})  # 根据推荐文章键执行查询操作
+    b_article_tb.close()  # 关闭数据库连接
+
+    if not res["valid"]:
+        # NOTE: 如果从数据库获取文章列表信息失败，则返回空列表
+        return {"detail": []}
+    return res
+
 
 def get_article_classify():
     """获取文章分类简要信息
@@ -34,10 +66,10 @@ def get_article_classify():
         }]
     }
     """
-    b_category_tb = base.Base("b_category") #实例化数据库操作基类
-    b_category_tb.connect() #连接数据库
-    res = b_category_tb.show() #执行查询操作
-    b_category_tb.close() #关闭数据库连接
+    b_category_tb = base.Base("b_category")  # 实例化数据库操作基类
+    b_category_tb.connect()  # 连接数据库
+    res = b_category_tb.show()  # 执行查询操作
+    b_category_tb.close()  # 关闭数据库连接
 
     category_info = []
     if res["valid"]:
@@ -46,10 +78,10 @@ def get_article_classify():
 
     # NOTE: 查询每个标签所对应的文章数
     for category in category_info:
-        b_article_tb = base.Base("b_article") #实例化数据库操作基类
-        b_article_tb.connect() #连接数据库
-        article_res = b_article_tb.show({"category_id": category["id"]}) #根据标签ID执行查询操作
-        b_article_tb.close() #关闭数据库连接
+        b_article_tb = base.Base("b_article")  # 实例化数据库操作基类
+        b_article_tb.connect()  # 连接数据库
+        article_res = b_article_tb.show({"category_id": category["id"]})  # 根据标签ID执行查询操作
+        b_article_tb.close()  # 关闭数据库连接
         if not article_res["valid"]:
             # NOTE: 如果从数据库获取文章信息失败，则给文章数赋值为0
             category["title_count"] = 0
@@ -58,6 +90,7 @@ def get_article_classify():
             category["title_count"] = len(article_res["detail"])
     res["detail"] = category_info
     return res
+
 
 def get_article_list():
     """获取文章列表
@@ -78,12 +111,12 @@ def get_article_list():
         }]
     }
     """
-    b_article_tb = base.Base("b_article") #实例化数据库操作基类
-    b_article_tb.connect() #连接数据库
-    res = b_article_tb.show() #执行查询操作
-    b_article_tb.close() #关闭数据库连接
+    b_article_tb = base.Base("b_article")  # 实例化数据库操作基类
+    b_article_tb.connect()  # 连接数据库
+    res = b_article_tb.show()  # 执行查询操作
+    b_article_tb.close()  # 关闭数据库连接
 
     if not res["valid"]:
         # NOTE: 如果从数据库获取文章列表信息失败，则返回空列表
-        return []
+        return {"detail": []}
     return res
--- pages/index/views.py
+++ pages/index/views.py
@@ -1,7 +1,8 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 from ..base.views import BaseView
 from flask_blog.model import api
+
 
 class IndexView(BaseView):
 
@@ -18,9 +19,14 @@ class IndexView(BaseView):
         article_classify_res = api.get_article_classify()
         article_classifies = article_classify_res["detail"]
 
+        # NOTE: 获取推荐文章列表
+        article_tuijian_res = api.get_recommend_article_lists()
+        article_tuijians = article_tuijian_res["detail"]
+
         return {"blog_name": "123",
                 "blog_description": "456",
                 "author": "wi",
                 "author_introduce": "testtesttest!",
                 "article_lists": article_lists,
-                "article_classifies": article_classifies}
+                "article_classifies": article_classifies,
+                "article_tuijians": article_tuijians}
