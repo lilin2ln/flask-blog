c31a39d28f05c03fdfb0a8edb8b99ccf5f4c3b94
lilin <lilin@capsheaf.com.cn>
2019-01-29 10:00:15 +0800

    #func-alter: 增加点击发布文章获取文章标签的功能

--- api_1_0/article.py
+++ api_1_0/article.py
@@ -2,10 +2,11 @@
 
 from flask import jsonify, request, url_for, g
 from . import api
+from flask_blog.model import api as model_api
 
 # 查询文章标签
 @api.route('/article/get_label')
 def get_label():
-    return jsonify({
-        '2': 3
-    })
+    article_classify_res = model_api.get_article_classify()
+    article_classifies = article_classify_res["detail"]
+    return jsonify(article_classifies)
